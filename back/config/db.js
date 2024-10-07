b093352b139b97dd3cb88a39a53b30f024c28bf8
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-07 14:09:00 +0200

    fix comment

--- back/config/db.js
+++ back/config/db.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-// Configurer les variables d'environnement dans un fichier .env
+// Config env
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  logging: false, // Pour éviter les logs des requêtes SQL dans la console
+  logging: false, // eviter logs sql
 });
 
 module.exports = sequelize;
