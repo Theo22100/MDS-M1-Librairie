fa19f14603efaa3ef3b1c9b857a5525d0449797a
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-08 12:24:22 +0200

    add comments

--- library-app/src/context/AuthContext.js
+++ library-app/src/context/AuthContext.js
@@ -1,29 +1,36 @@
 import React, { createContext, useState, useEffect } from 'react';
 
+// Création du contexte d'authentification
 export const AuthContext = createContext();
 
+// Composant fournisseur d'authentification
 const AuthProvider = ({ children }) => {
+  // État qui détermine si l'utilisateur est authentifié
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  // Vérifie si un token est présent dans le localStorage au chargement du composant
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token'); // Recup token
     if (token) {
-      setIsAuthenticated(true);  // Forcer la mise à jour ici
+      setIsAuthenticated(true); 
     } else {
-      console.log('Pas de token trouvé, isAuthenticated reste false');
+      console.log('Pas de token trouvé, isAuthenticated reste false'); 
     }
-  }, []);
+  }, []); // L'effet ne s'exécute qu'une seule fois, au montage du composant
 
+  // Fonction pour se connecter et stocker le token dans localStorage
   const login = (token) => {
-    localStorage.setItem('token', token);
-    setIsAuthenticated(true);
+    localStorage.setItem('token', token); 
+    setIsAuthenticated(true); 
   };
 
+  // Fonction pour se déconnecter et supprimer le token de localStorage
   const logout = () => {
-    localStorage.removeItem('token');
-    setIsAuthenticated(false);
+    localStorage.removeItem('token'); 
+    setIsAuthenticated(false); 
   };
 
+  // Fournit les fonctions et l'état d'authentification à tous les composants enfants
   return (
     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
