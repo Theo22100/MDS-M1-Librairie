b21293a172d33b30f4432b9b502c91ecc052e803
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-08 12:24:58 +0200

    add redirection home if user logged

--- library-app/src/pages/RegisterPage.js
+++ library-app/src/pages/RegisterPage.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 import RegisterForm from '../components/UserForm/Registerform';
 
 const RegisterPage = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/home'); // Rediriger vers home si déjà connecté
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <div>
-      <h1>Inscription</h1>
+      <h2>Inscription</h2>
       <RegisterForm />
     </div>
   );
