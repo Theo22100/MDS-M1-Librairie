32b0f8440fad2c9502af42774864c22eceab0e72
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-10 11:23:15 +0200

    add button admin

--- library-app/src/components/Header/header.js
+++ library-app/src/components/Header/header.js
@@ -1,49 +1,69 @@
-import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../context/AuthContext';
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Header = () => {
-  const { isAuthenticated, isAdmin, logout } = useContext(AuthContext); 
+  const { isAuthenticated, isAdmin, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link to="/home" className="navbar-brand">Bibliothèque</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <Link to="/home" className="navbar-brand">
+            Bibliothèque
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
-              
               {isAuthenticated ? (
                 <>
                   {/* Si Connecté */}
                   <li className="nav-item">
-                    <Link to="/home" className="nav-link">Accueil</Link>
+                    <Link to="/home" className="nav-link">
+                      Accueil
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/loans" className="nav-link">Emprunts</Link>
+                    <Link to="/loans" className="nav-link">
+                      Emprunts
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/reservations" className="nav-link">Réservations</Link>
+                    <Link to="/reservations" className="nav-link">
+                      Réservations
+                    </Link>
                   </li>
 
                   {/* Si Admin */}
                   {isAdmin && (
                     <li className="nav-item">
-                      <Link to="/admin" className="nav-link">Admin</Link>
+                      <Link to="/admin" className="btn btn-primary">
+                        Admin
+                      </Link>
                     </li>
                   )}
 
                   <li className="nav-item">
-                    <button className="btn btn-danger ms-2" onClick={handleLogout}>
+                    <button
+                      className="btn btn-danger ms-2"
+                      onClick={handleLogout}
+                    >
                       Déconnexion
                     </button>
                   </li>
@@ -52,10 +72,14 @@ const Header = () => {
                 <>
                   {/* Pas Connecté */}
                   <li className="nav-item">
-                    <Link to="/login" className="nav-link">Connexion</Link>
+                    <Link to="/login" className="nav-link">
+                      Connexion
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/register" className="nav-link">Inscription</Link>
+                    <Link to="/register" className="nav-link">
+                      Inscription
+                    </Link>
                   </li>
                 </>
               )}
