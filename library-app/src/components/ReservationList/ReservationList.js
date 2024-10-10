55d55cdf142e0b798e9fb0682077d4fbe17420ce
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-10 10:12:31 +0200

    add reservation front

--- library-app/src/components/ReservationList/ReservationList.js
+++ library-app/src/components/ReservationList/ReservationList.js
@@ -1,32 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { getAllReservations } from '../../services/reservationService';
+import React, { useEffect, useState, useContext } from 'react';
+import { getReservationsUser } from '../../services/reservationService';
 import ReservationListItem from './ReservationListItem';
-// NON FINI
-const ReservationList = () => {
+import { AuthContext } from '../../context/AuthContext'; 
+import { jwtDecode } from 'jwt-decode'; 
+
+const LoanList = () => {
+  // État pour stocker reservations récupérés
   const [reservations, setReservations] = useState([]);
+  // Récupération authentification contexte
+  const { isAuthenticated } = useContext(AuthContext);
 
   useEffect(() => {
+    // Récupérer reservations de user
     const fetchReservations = async () => {
       try {
-        const data = await getAllReservations();
-        setReservations(data);
+        const token = localStorage.getItem('token'); 
+        if (token) {
+          const decodedToken = jwtDecode(token); 
+          const userId = decodedToken.id; 
+
+          // récupérer les reservations
+          const data = await getReservationsUser(userId);
+          setReservations(data); 
+        }
       } catch (error) {
-        console.error('Erreur lors de la récupération des réservations :', error);
+        console.error('Erreur lors de la récupération des réservations :', error); 
       }
     };
 
-    fetchReservations();
-  }, []);
+    // récupération reservations si user connecté
+    if (isAuthenticated) {
+      fetchReservations();
+    }
+  }, [isAuthenticated]); // relancer l'effet lors de la modification
 
   return (
     <div className="row">
-      <div className="col-md-6 mb-4">
-        {reservations.map((reservation) => (
-          <ReservationListItem key={reservation.id} reservation={reservation} />
-       ))}
-      </div>
+      {reservations.length > 0 ? (
+        // Liste si réservation existe
+        reservations.map((reservation) => (
+          <div key={reservation.id} className="col-md-6 mb-4">
+            <ReservationListItem reservation={reservation} /> 
+          </div>
+        ))
+      ) : (
+        // Aucune réservation trouvé
+        <p className="text-center">Aucune Réservation trouvé.</p>
+      )}
     </div>
   );
 };
 
-export default ReservationList;
+export default LoanList;
--- library-app/src/components/ReservationList/ReservationListItem.js
+++ library-app/src/components/ReservationList/ReservationListItem.js
@@ -1,19 +1,13 @@
-import React from "react";
-
 const ReservationListItem = ({ reservation }) => {
   return (
     <div className="card shadow-sm">
       <div className="card-body">
         <h3 className="card-title">Réservation</h3>
         <p className="card-text">
-        <strong>Utilisateur :</strong> {reservation.user.name} {reservation.user.firstname}
+          <strong>Livre :</strong> {reservation.book.title} - {reservation.book.author}
         </p>
         <p className="card-text">
-        <strong>Livre :</strong> {reservation.book.title} - {reservation.book.author}
-        </p>
-        <p className="card-text">
-        <strong>Date de réservation :</strong>{" "}
-          {new Date(reservation.reservation_date).toLocaleDateString()}
+          <strong>Date de Réservation :</strong> {new Date(reservation.reservation_date).toLocaleDateString()}
         </p>
       </div>
     </div>
